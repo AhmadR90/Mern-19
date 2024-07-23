0d00a38cdefe8a7ee9fbad1423dc49967d456f73
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-07-23 11:30:23 +0500

    variable js 23-7-2024

---
+++ variable.js
@@ -0,0 +1,38 @@
+//Constant
+
+// const a=1;
+// console.log("a before scope",a)
+
+// {
+// console.log("a before scope", a);
+// //const a=2;
+// }
+
+// console.log("a before scope", a);
+
+//let case 1
+
+// let b ;
+// console.log("b before scope", b);
+
+// {
+//   b="new value"
+//   console.log("b before scope", b);
+//   //const a=2;
+// }
+
+// console.log("b before scope", b);
+
+//let case 2
+
+// let a = 1;
+// console.log("b in outer scope");
+// {
+//   let a = 2;
+//   console.log("b in block scope");
+//   a = "new value 3 in block";
+//   console.log(a);
+// }
+// console.log("a after block", a);
+
+
