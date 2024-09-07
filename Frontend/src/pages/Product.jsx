79d86e8e9e24ab1eeb340ff4afdba1f6d920e610
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-09-07 09:29:29 +0500

    searching products

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,32 +1,54 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import ProductCard from "./ProductCard"
+import ProductCard from "./ProductCard";
+import { productData } from "../data";
 const Product = () => {
   const [product, setProduct] = useState([]);
+
+  const [input, setInput] = useState([]);
+
   const fetchProducts = async () => {
     const items = await axios.get("https://fakestoreapi.com/products");
-    console.log(items.data);
+
     setProduct(items.data);
+    setInput(items.data);
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
+  const inputHandler = (e) => {
+    const val = e.target.value;
+    setInput(val);
+    const searchedProducts = product.filter((item, index) => {
+      if (item.title.toLowerCase().includes(val.toLowerCase())) {
+        return true;
+      }
+    });
+    setInput(searchedProducts);
+  };
 
   return (
     <>
-      {product.map((item, index, arr) => {
-        return <>
-        <ProductCard title={item.title} img={item.image} cat={item.category} price={item.price} des={item.description} rate={item.rating.rate} count={item.rating.count}/>
-          {/* <h1>{item.title}</h1>
-          <img src={item.image} alt="" />
-          <p>{item.category}</p>
-          <p>{item.price}</p>
-          <p>{item.description}</p>
-          <p>{item.rating.rate}</p> */}
-        
-        </>;
+      <input placeholder="Enter the value" onChange={inputHandler} />
+      <br />
+
+      {input.map((item, index, arr) => {
+        return (
+          <>
+            <ProductCard
+              key={item.id}
+              title={item.title}
+              img={item.image}
+              cat={item.category}
+              price={item.price}
+              des={item.description}
+              rate={item.rating.rate}
+              count={item.rating.count}
+            />
+          </>
+        );
       })}
     </>
   );
--- Frontend/src/pages/ProductCard.jsx
+++ Frontend/src/pages/ProductCard.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const ProductCard = (props) => {
   return (
-    <div className="flex flex-wrap bg-stone-600">
-     
-        <h1>{props.title}</h1>
-        <img src={props.img} alt="" />
-        <h3>{props.cat}</h3>
-        <h2>{props.price}</h2>
-        <p>{props.des}</p>
-        <p>{props.rate}</p>
-        <p>{props.count}</p>
-    
+    <div className="flex-row p-4 bg-green-400 border-red-800 border-4 w-[600px] h-[400px] ">
+      <img src={props.img} alt="" className="h-40 w-40" />
+      <h1 className="font-bold">{props.title}</h1>
+      <h3>{props.cat}</h3>
+      <h2>{props.price}</h2>
+      <p className="text-wrap">{props.des}</p>
+      <p>{props.rate}</p> 
+      <p>{props.count}</p>
+      {/* <button className="bg-"
+        onClick={() => {
+          const updatedProduct = [
+            // ...props.product.slice(0, props.id),
+            ...props.product.pop(props.id)
+            // ...props.product.slice(0,props.id + 1),
+          ];
+         props.setsProduct(updatedProduct);
+        }}
+      >
+        Delete
+      </button> */}
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
