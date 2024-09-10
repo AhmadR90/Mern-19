e1289b7e0202b5202e1bbcb4aa41ddd787cd2820
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-09-10 12:47:18 +0500

    react till routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,18 @@
 // import Home from "./pages/Home";
 import Product from "./pages/Product";
-
-
+import {Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Error from "./pages/Error";
+import Navbar from "./pages/Navbar";
 const App = () => {
   return <>
-  
-    {/* <Home /> */}
-    <Product/>
+    <Navbar/>
+     <Routes>
+      <Route path="/home" element={<Home/>}></Route>
+      <Route path="/products" element={<Product/>}></Route>
+      <Route path="*" element={<Error/>}></Route>
+     </Routes>
     
-    {/* <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1> */}
  
   </>;
 };
---
+++ Frontend/src/pages/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/products">Products</NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
